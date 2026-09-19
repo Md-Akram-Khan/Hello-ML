b5c5015b1f2afceffbddbf0a115fa1ca1c6224a4
Md-Akram-Khan <thankyouakramkhan@gmail.com>
2026-09-20 01:48:42 +0600

    updated lr_utils.py

--- lr_utils.py
+++ lr_utils.py
@@ -1,19 +1,25 @@
 import numpy as np
 import h5py
-    
-    
+
 def load_dataset():
     train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
-    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
-    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+    # print(train_dataset.keys()) # ['list_classes', 'train_set_x', 'train_set_y']
+    
+    train_set_x_data = np.array(train_dataset["train_set_x"][:])
+    # print(str(train_set_x_data.shape)) # (209, 64, 64, 3)
+    
+    train_set_y_data = np.array(train_dataset["train_set_y"][:])
+    # print(str(train_set_y_data.shape)) # (209,)
+    
+    train_set_y_data = train_set_y_data.reshape((1, train_set_y_data.shape[0]))
+    # print(str(train_set_y_data.shape)) # (1, 209)
+
 
     test_dataset = h5py.File('datasets/test_catvnoncat.h5', "r")
-    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
-    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+    test_set_x_data = np.array(test_dataset["test_set_x"][:]) 
+    test_set_y_data = np.array(test_dataset["test_set_y"][:])
+    test_set_y_data = test_set_y_data.reshape((1, test_set_y_data.shape[0])) 
+    
+    classes = np.array(test_dataset["list_classes"][:])
 
-    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
-    
-    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
-    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
-    
-    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+    return train_set_x_data, train_set_y_data, test_set_x_data, test_set_y_data, classes
